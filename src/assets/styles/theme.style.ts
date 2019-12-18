d4ca5514ded14a76b7f14b05d122c34f08ba2cfa
charliehay <charlie.hay@telus.com>
2019-12-18 18:20:06 -0500

    feat(tm): header menu

--- src/assets/styles/theme.style.ts
+++ src/assets/styles/theme.style.ts
@@ -2,7 +2,7 @@ import { Appearance } from 'react-native-appearance'
 import { Dimensions } from 'react-native'
 const { width } = Dimensions.get('window')
 const guidelineWidth = 350
-const darkMode = Appearance.getColorScheme() === 'dark'
+const darkMode = Appearance.getColorScheme() === 'dark' || Appearance.getColorScheme() === 'no-preference'
 const scale = (size) => `${(width / guidelineWidth) * size}px`
 
 const PURPLE = `rgba(96, 57, 169, 1)`
@@ -13,11 +13,12 @@ const WHITE = `#EFF3F8`
 
 export default {
   BACKGROUND_COLOR: darkMode ? DARK_GRAY : WHITE,
-  FOREGROUND_COLOR:  darkMode ? DARK_GRAY : WHITE,
+  FOREGROUND_COLOR: darkMode ? DARK_GRAY : WHITE,
   PRIMARY_COLOR: darkMode ? WHITE : DARK_GRAY,
   SECONDARY_COLOR: darkMode ? DARK_GRAY : WHITE,
   HIGHLIGHT_COLOR: PURPLE,
   HEADER_COLOR: darkMode ? '#4b286d' : '#FFF',
+  MENU_ICON_COLOR: darkMode ? '#FFF' : '#4b286d',
   JOB_CHART_COLOR: '#2B8001',
   MANAGER_CHART_COLOR: '#FACA69',
   PERFORMANCE_CHART_COLOR: '#4B286D',
--- src/components/Header/Header.style.ts
+++ src/components/Header/Header.style.ts
@@ -1,17 +1,32 @@
 import styled from 'styled-components'
 import theme from 'assets/styles/theme.style'
-import { View, Image } from 'react-native'
+import { View, Image, Text } from 'react-native'
 
 export const Wrapper = styled(View)({
   height: `64px`,
   width: `100%`,
   display: 'flex',
+  flexDirection: 'row',
   alignItems: 'center',
-  justifyContent: 'center',
+  justifyContent: 'space-between',
+  padding: `0px 20px`,
   backgroundColor: theme.HEADER_COLOR
 })
 
 export const Logo = styled(Image)({
   height: 20,
   width: 150
+})
+
+export const Menu = styled(Text)({
+  fontSize: 30,
+  color: theme.MENU_ICON_COLOR,
+  paddingTop: 5
+})
+
+export const Profile = styled(View)({
+  height: 25,
+  width: 25,
+  borderRadius: `15px`,
+  background: `rgba(0,0,0,0.5)`
 })

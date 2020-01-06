e09976d29c7a9f589cab3911322106b26e47add0
charliehay <charlie.hay@telus.com>
2020-01-06 11:12:01 -0500

    feat(tme): fixed login issue

--- src/screens/LoginScreen/LoginScreen.component.tsx
+++ src/screens/LoginScreen/LoginScreen.component.tsx
@@ -3,8 +3,9 @@ import { useDispatch } from 'react-redux'
 import { withNavigation } from 'react-navigation'
 import theme from 'assets/styles/theme.style'
 import Loading from 'components/Loading'
+import { format } from 'date-fns'
 import { TOGGLE_AUTH } from 'services/redux'
-import { auth } from 'services/firebase'
+import { auth, db } from 'services/firebase'
 import { validateEmail } from 'utils/helpers/validate'
 import * as S from './LoginScreen.style'
 
@@ -22,6 +23,18 @@ const LoginScreen = ({ initialized }) => {
         const { user } = await auth.signInWithEmailAndPassword(emailInput, passwordInput)
         const { providerData, uid } = user
         const { displayName, email, photoUrl } = providerData
+
+        const responses = await db.doc(`pulsecheck/${uid}/responses/job_satisfaction`).get()
+        const data = await responses.data()
+        if (!data) {
+          const batch = db.batch()
+          const date = format(new Date().getTime(), `yyyy-MM-dd`)
+          batch.set(db.doc(`pulsecheck/${uid}/responses/job_satisfaction`), { [`${date}`]: 0 })
+          batch.set(db.doc(`pulsecheck/${uid}/responses/manager_approval`), { [`${date}`]: 0 })
+          batch.set(db.doc(`pulsecheck/${uid}/responses/my_performance`), { [`${date}`]: 0 })
+          batch.set(db.doc(`pulsecheck/${uid}/responses/team_rating`), { [`${date}`]: 0 })
+          await batch.commit()
+        }
         return dispatch({ type: TOGGLE_AUTH, status: true, value: { uid, displayName, email, photoUrl } })
       } catch (err) {
         return setPasswordError('Invalid Email or Password')
